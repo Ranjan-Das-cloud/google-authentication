fb5423fe34ff79b11f5ecc5e5ecd488fdd1a2af7
Ranjan-Das-cloud <ranjan.rd08@gmail.com>
2020-09-25 22:19:26 +0530

    Updated JavaScript app file

---
+++ app.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDz2d21rW2O-VyL1JkHeuoAfY6bVkxZAYo",
+    authDomain: "finnman-expanse-tracker.firebaseapp.com",
+    databaseURL: "https://finnman-expanse-tracker.firebaseio.com",
+    projectId: "finnman-expanse-tracker",
+    storageBucket: "finnman-expanse-tracker.appspot.com",
+    messagingSenderId: "459605259773",
+    appId: "1:459605259773:web:84f070921b6fd186339594"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
